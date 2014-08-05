0a3e8570caf09ab2f56d3f4ce456f6821d0b8fca
nobermeijer <n.obermeijer@recognize.nl>
2014-08-05 15:00:27 +0200

    Added simple implementation for json response error handling

--- Component/HttpFoundation/JsonResponse.php
+++ Component/HttpFoundation/JsonResponse.php
@@ -12,6 +12,14 @@ use Symfony\Component\HttpFoundation\JsonResponse as BaseJsonResponse;
 class JsonResponse extends BaseJsonResponse {
 
 	/**
+	 * @param $data
+	 * @return string
+	 */
+	protected function jsonEncode($data) {
+		return json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_NUMERIC_CHECK);
+	}
+
+	/**
 	 * Overrides Symfony's jsonresponse set method
 	 *
 	 * @param array $data
@@ -19,9 +27,18 @@ class JsonResponse extends BaseJsonResponse {
 	 * @throws \InvalidArgumentException
 	 */
 	public function setData($data = array()) {
-		$this->data = json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_NUMERIC_CHECK);
+		$this->data = $this->jsonEncode($data);
 
 		return $this->update();
+	}
+
+	/**
+	 * @param mixed $contents
+	 * @return \Symfony\Component\HttpFoundation\Response|void
+	 */
+	public function setContent($contents) {
+		if(is_object($contents) || is_array($contents)) $contents = $this->jsonEncode($contents);
+		parent::setContent($contents);
 	}
 
 }
---
+++ EventListener/ExceptionListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Recognize\ExtraBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent,
+	Symfony\Component\HttpFoundation\Response;
+
+use Recognize\ExtraBundle\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class ExceptionListener
+ * @package Recognize\ExtraBundle\EventListener
+ * @author Nick Obermeijer <n.obermeijer@recognize.nl>
+ */
+class ExceptionListener {
+
+	/**
+	 * @param \Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent $event
+	 * @return int
+	 */
+	protected function getStatusCode(GetResponseForExceptionEvent $event = null) {
+		if($response = $event->getResponse()) { // When response is set...
+			return ($response->getStatusCode() != 0) ? $response->getStatusCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+		}
+		return ($event->getException()->getCode() != 0) ? $event->getException()->getCode() : Response::HTTP_INTERNAL_SERVER_ERROR;
+	}
+
+	/**
+	 * @param GetResponseForExceptionEvent $event
+	 */
+	public function onKernelException(GetResponseForExceptionEvent $event) {
+		if(0 === strpos($event->getRequest()->headers->get('Content-Type'), 'application/json')) {
+			$exception = $event->getException(); // Exception
+
+			$jsonResponse = new JsonResponse();
+			$jsonResponse->setStatusCode($this->getStatusCode($event));
+			$jsonResponse->setData(array(
+				'status' => 'failed',
+				'data' => array('message' => $exception->getMessage())
+			));
+
+			$event->setResponse($jsonResponse);
+		}
+	}
+
+}
