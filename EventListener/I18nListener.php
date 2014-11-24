e3822088334367b82fe072faf96c8fbb6cc3b8e0
nobermeijer <n.obermeijer@recognize.nl>
2014-11-24 16:46:55 +0100

    Fixed minor issue where _route_params has not been set

--- EventListener/I18nListener.php
+++ EventListener/I18nListener.php
@@ -15,10 +15,11 @@ class I18nListener {
 	 * @param GetResponseEvent $event
 	 */
 	public function onKernelRequest(GetResponseEvent $event) {
-		$routeParams = $event->getRequest()->get('_route_params');
-		if(array_key_exists('_locale', $routeParams)) {
-			unset($routeParams['_locale']); // Remove from route params
-			$event->getRequest()->attributes->set('_route_params', $routeParams);
+		if($routeParams = $event->getRequest()->get('_route_params')) {
+			if(array_key_exists('_locale', $routeParams)) {
+				unset($routeParams['_locale']); // Remove from route params
+				$event->getRequest()->attributes->set('_route_params', $routeParams);
+			}
 		}
 	}
 
