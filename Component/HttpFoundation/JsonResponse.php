1eee38942df52fb1715972832433d1dce2b2c476
Willem Slaghekke <w.slaghekke@recognize.nl>
2015-05-18 17:17:20 +0200

    more changes to fixnonnumericfloatvals

--- Component/HttpFoundation/JsonResponse.php
+++ Component/HttpFoundation/JsonResponse.php
@@ -16,22 +16,28 @@ class JsonResponse extends BaseJsonResponse {
 	 * @return string
 	 */
 	protected function jsonEncode($data) {
-//		$this->fixNonNumericFloatValues($data);
-		return json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
+		$newData = $this->fixNonNumericFloatValues($data);
+		return json_encode($newData, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
 	}
 
-	/**
-	 * @param array $data
-	 */
-	protected function fixNonNumericFloatValues(&$data) {
+    /**
+     * @param array $data
+     * @return array
+     */
+	protected function fixNonNumericFloatValues($data) {
+        $newData = array();
 		foreach($data as $key => $value) {
-			if(is_array($value) || is_object($value)) $this->fixNonNumericFloatValues($value);
-			else { // I frown upon this...
+			if(is_array($value) || is_object($value)) {
+                $newData[$key] = $this->fixNonNumericFloatValues($value);
+            } else { // I frown upon this...
 				if(is_string($value) && is_numeric($value) && ($value == 0 || (substr($value, 0, 1) !== '0' && substr($value, 0, 1) != '+'))) {
-					$data[$key] = (preg_match('/\d+\.\d+/', $value)) ? (float)$value : (int)$value;
-				}
+					$newData[$key] = (preg_match('/\d+\.\d+/', $value)) ? (float)$value : (int)$value;
+				} else {
+                    $newData[$key] = $value;
+                }
 			}
 		}
+        return $newData;
 	}
 
 	/**
